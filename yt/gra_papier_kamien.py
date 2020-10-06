ed98d8db40fff92cf7c9fa333316632b8b13732e
Marcin Dyrdół <marcindyrdol@gmail.com>
2020-10-06 23:57:25 +0200

    Refactor game rock paper scissors

--- yt/gra_papier_kamien.py
+++ yt/gra_papier_kamien.py
@@ -15,15 +15,12 @@ def battle(first_player,second_player):
     if (first_player.lower() == second_player.lower()):
         print("Draw")
         return 0
-    elif first_player.lower() == 'rock' and second_player.lower() == 'scissors' \
-            or first_player.lower() == 'scissors' and second_player.lower() == 'paper' \
-            or first_player.lower() == 'paper' and second_player.lower() == 'rock':
-        print("Player 1 won round")
-        return 1
-    else:
-        print("Player 2 won round")
-        return -1
-
+    wynik = {
+        ('rock','scissors'):1,
+        ('scissors','paper'):1,
+         ('paper','rock'):1
+        }
+    return wynik.get((first_player,second_player),-1)
 #------------------------------------------------------------------------------------------
 
 while result_first_player!=3 and result_second_player!=3:
@@ -32,8 +29,10 @@ while result_first_player!=3 and result_second_player!=3:
     second_player = choice("Player 2")
     result = battle(first_player,second_player)
     if result == 1:
+        print("Player 1 won round")
         result_first_player +=1
     elif result == -1:
+        print("Player 2 won round")
         result_second_player +=1
 
 if result_first_player > 2:
